6b6853401a46a5707058812a720acf1f1e5b7080
Alex <kangthink@insighter.co.kr>
2021-12-07 15:11:21 +0900

    Delete unused files and change the style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import './App.css'
 
 import Home from './Home'
 import QuizSession from './QuizSession'
@@ -8,14 +7,14 @@ import WordList from './WordList'
 
 function App() {
   return (
-    <div className='App'>
+    <section style={{ textAlign: 'center' }}>
       <h1>다풀자 영단어</h1>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='quiz' element={<QuizSession />} />
         <Route path='wordlist' element={<WordList />} />
       </Routes>
-    </div>
+    </section>
   )
 }
 
