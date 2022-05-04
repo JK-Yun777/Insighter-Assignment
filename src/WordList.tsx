8a9545a38fc4978a8aec483ff35dd5eed6a04645
JK-Yun777 <ramieta16@gmail.com>
2022-05-04 16:15:25 +0900

    [Feat]: Add fetching wordlist logic

--- src/WordList.tsx
+++ src/WordList.tsx
@@ -1,4 +1,25 @@
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+const getWords = async () => {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: `/vocabs.json`
+    })
+
+    return response
+  } catch (e) {
+    if (e instanceof TypeError) {
+      console.log('TypeError')
+    } else if (e instanceof SyntaxError) {
+      console.log('SyntaxError')
+    } else if (typeof e === 'string') {
+      console.log('string')
+    }
+  }
+}
 
 interface Word {
   text: string
@@ -14,10 +35,31 @@ function WordView(word: Word) {
 }
 
 function WordList() {
+  const [wordlist, setWordList] = useState([])
   const linkStyle = {
     display: 'block',
     padding: '8px'
   }
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await getWords()
+
+        setWordList(() => response?.data)
+      } catch (e) {
+        if (e instanceof TypeError) {
+          console.log('TypeError')
+        } else if (e instanceof SyntaxError) {
+          console.log('SyntaxError')
+        } else if (typeof e === 'string') {
+          console.log('string')
+        }
+      }
+    }
+
+    fetchData()
+  }, [])
 
   // TODO
   // 훅을 이용해서, 화면이 로드되면 아래 주소에서 단어를 들고와서 화면에 표시
@@ -26,11 +68,11 @@ function WordList() {
   // warning!
   // 만약 어떠한 이유로 작동이 되지 않는다면, 그 문제를 우회해서
   // 전체 기능이 동작하도록 코드를 구현.
-  const wordlist: Word[] = [
-    { text: 'apple', meaning: 'n. 사과' },
-    { text: 'brick', meaning: 'n. 벽돌' },
-    { text: 'leap', meaning: 'v. 뛰다, 급증하다' }
-  ]
+  // const wordlist: Word[] = [
+  //   { text: 'apple', meaning: 'n. 사과' },
+  //   { text: 'brick', meaning: 'n. 벽돌' },
+  //   { text: 'leap', meaning: 'v. 뛰다, 급증하다' }
+  // ]
 
   return (
     <section>
